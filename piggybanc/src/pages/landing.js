0eb0c15d3787d144cec48521125869ab4e7e03ac
Monique <monique.senjaya@binus.ac.id>
2022-03-17 17:15:19 +0700

    making a landing page

---
+++ piggybanc/src/pages/landing.js
@@ -0,0 +1,28 @@
+import logo from '../logo.svg';
+import Navbar from '../components/landing/navbar';
+function Landing() {
+  return (
+    
+    <div className="bg-[#08143D] h-[100%] w-[100%]">
+      
+      <div className='flex justify-end z-10 '>
+        <span className="bg-[#273250] absolute h-[75%] w-[70%] rounded-bl-[95%]"/>
+      </div>
+
+      <div className='relative z-20 font-Inter text-white'>
+          <Navbar/>
+      </div>
+
+      <div className='h-[4000px] border-1'>
+
+      </div>
+
+      
+      
+       
+    
+    </div>
+  );
+}
+
+export default Landing;
