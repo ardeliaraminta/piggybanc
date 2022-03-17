aa29dde2e26d376c9b5a71d833dde4b907863b63
Monique <monique.senjaya@binus.ac.id>
2022-03-17 17:15:11 +0700

    adding fonts

--- piggybanc/tailwind.config.js
+++ piggybanc/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        'sans':['Inter'],
-        'Inter':['Inter']
+        Inter: ['Inter', 'sans-serif']
       },
     },
   },
