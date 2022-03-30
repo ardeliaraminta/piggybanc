71d495a7d1d8dc4307097e4596d446368c6814ad
Ardelia <71622783+ardeliaraminta@users.noreply.github.com>
2022-03-31 02:39:44 +0700

    1/2

--- piggybanc/src/components/landing/team.js
+++ piggybanc/src/components/landing/team.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import linkedin from '../../assets/linkedin.png';
 // to do list : make it mobile friendly 
 
 export default function Team() {
@@ -10,8 +11,9 @@ export default function Team() {
             <p className='text-3xl py-6 text-gray-500'></p>
         </div>
 
-        <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
-            <div className='border py-8 rounded-xl shadow-xl' >
+        <div className='grid md:grid-cols-3 gap-6 px-2 text-center'>
+            <div className='border py-8 rounded-l shadow-xl' >
+            <img class="pt-2 mt-4 hero container max-w-screen-lg mx-auto pb-10 flex w-[160px] h-[200px] md:w-[176px] md:h-[198px]" src={linkedin}></img>
                 <p className='text-6xl font-bold text-indigo-600'></p>
                 <p className='text-gray-400 mt-2'>Monique</p>
             </div>
@@ -20,6 +22,7 @@ export default function Team() {
                 <p className='text-gray-400 mt-2'>Github</p>
             </div>
             <div className='border py-8 rounded-xl shadow-xl' >
+            <img class="pt-2 mt-4 hero container max-w-screen-lg mx-auto pb-10 flex w-[200px] h-[230px] md:w-[176px] md:h-[198px]" src={linkedin}></img>
                 <p className='text-6xl font-bold text-indigo-600'></p>
                 <p className='text-gray-400 mt-2'>Ardelia</p>
             </div>
