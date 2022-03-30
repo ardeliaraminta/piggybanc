97b824e7690e4b4ca37b909bf2a195d489c503e7
Ardelia <71622783+ardeliaraminta@users.noreply.github.com>
2022-03-30 20:33:08 +0700

    why done

--- piggybanc/src/components/landing/footer.js
+++ piggybanc/src/components/landing/footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import linkedin from '../../assets/linkedin.png';
 
 export default function Footer() {
   return (
-
-<div class="flex flex-col h-screen justify-between">
-  <footer class="h-10">Copyright © 2022 PiggyBanc Association</footer>
-</div>
-  )
-
+    <div class="flex flex-col h-screen justify-between">
+    <footer class="h-10">Copyright © 2022 PiggyBanc Association</footer>
+  </div>
+  
+     )
 }
