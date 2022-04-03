911b2b888daa1e42dba8e23258214c8853216dfc
Ardelia <71622783+ardeliaraminta@users.noreply.github.com>
2022-04-03 23:48:53 +0700

    team

--- piggybanc/src/components/landing/team.js
+++ piggybanc/src/components/landing/team.js
@@ -5,14 +5,14 @@ import linkedin from '../../assets/linkedin.png';
 export default function Team() {
   return (
     <div name='about' className='w-full my-32'>
-    <div className='max-w-[1240px] mx-auto'>
+    <div className='max-w-[1000px] max-h-[500px] mx-auto'>
         <div className='text-center'>
             <h2 className='text-5xl font-bold'>Check us out</h2>
             <p className='text-3xl py-6 text-gray-500'></p>
         </div>
 
         <div className='grid md:grid-cols-3 gap-6 px-2 text-center'>
-            <div className='border py-8 rounded-l shadow-xl' >
+            <div className='border py-8 rounded-xl shadow-xl' >
             <img class="pt-2 mt-4 hero container max-w-screen-lg mx-auto pb-10 flex w-[160px] h-[200px] md:w-[176px] md:h-[198px]" src={linkedin}></img>
                 <p className='text-6xl font-bold text-indigo-600'></p>
                 <p className='text-gray-400 mt-2'>Monique</p>
