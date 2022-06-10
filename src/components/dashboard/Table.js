663840f91fb135fb7fb340970e3471cb3d55574c
Ardelia <71622783+ardeliaraminta@users.noreply.github.com>
2022-06-10 23:36:08 +0700

    get transaction

--- src/components/dashboard/Table.js
+++ src/components/dashboard/Table.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from "mdbreact";
 import Button from '@mui/material/Button';
 import axios from 'axios';
-import { useState } from 'react';
-import {useCookies} from 'react-cookie';
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { async } from '@firebase/util';
 
 const Table = () => {
-  const [details, setDetails] = useState("") ;
-  const [category, setCategory] = useState("") ;
-  const [amount, setAmount] = useState("") ;
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
   const [type, setType] = useCookies("");
   const [cookies, setCookies] = useCookies("email");
   const [message, setMessage] = useState('');
@@ -22,7 +23,7 @@ const Table = () => {
         amount: amount,
         type: type,
       }, {
-        headers: {"Authorization": cookies.token}
+        headers: { "Authorization": cookies.token }
       })
       console.log(res)
       setMessage("Successfully Added.")
@@ -32,76 +33,121 @@ const Table = () => {
       console.log(err)
     }
   };
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const res = await axios.get("https://piggybanc-backend.herokuapp.com/tx", {
+          headers: {
+            email: cookies.email
+          }
+        })
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+  }, [])
+
+
+
+  const createTx = async () => {
+    try {
+      const res = await axios.post("https://piggybanc-backend.herokuapp.com/tx", {
+        email: cookies.email,
+        details: "",
+        category: "",
+        amount: "",
+        type: "",
+      })
+   
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
+
+
   return (
     <>
-    <div className='dashprof'>
-          <h1 className='text-white'> Add New Entry</h1>
-          
+      <div className='dashprof'>
+        <h1 className='text-white'> Add New Entry</h1>
+
         <div className='grid md:grid-cols-3 gap-3 px-2 text-center'>
-            <div className='border py-8 rounded-xl shadow-xl' >
-                <p className='text-6xl font-bold text-indigo-600'></p>
-                <p className='text-gray-400 mt-2'>
-                <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form>
-              <MDBInput
-                label="Details"
-                group
-                type="password"
-                validate
-                error="wrong"
-                success="right"
-              />
-               <MDBInput
-               label="Category"
-                group
-                type="text"
-                validate
-                error="wrong"
-                success="right"
-                />
-                 <MDBInput
-                label="Amount"
-                group
-                type="text"
-                validate
-                error="wrong"
-                success="right"
-              />
-               <MDBInput
-               label="Type"
-                group
-                type="text"
-                validate
-                error="wrong"
-                success="right"
-                />
-               <Button
-    style={{
-        borderRadius: 30,
-        backgroundColor: "black",
-        padding: "12px 18px",
-        fontSize: "12px",
-    }}
-    variant="contained"
-    >
-    Add
-</Button>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-                </p>
+          <div className='border py-8 rounded-xl shadow-xl' >
+            <p className='text-6xl font-bold text-indigo-600'></p>
+            <p className='text-gray-400 mt-2'>
+              <MDBContainer>
+                <MDBRow>
+                  <MDBCol md="6">
+                    <form>
+                      <MDBInput
+                        label="Details"
+                        onChange={(e) => setDetails(e.currentTarget.value)}
+                        group
+                        type="password"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Category"
+                        onChange={(e) => setCategory(e.currentTarget.value)}
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Amount"
+                        onChange={(e) => setAmount(e.currentTarget.value)}
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Type"
+                        onChange={(e) => setType(e.currentTarget.value)}
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <Button
+                        onSubmit={ async (e) => createTx()}
+                        style={{
+                          borderRadius: 30,
+                          backgroundColor: "black",
+                          padding: "12px 18px",
+                          fontSize: "12px",
+                        }}
+                        variant="contained"
+                      >
+                        Add
+                      </Button>
+                    </form>
+                  </MDBCol>
+                </MDBRow>
+              </MDBContainer>
+            </p>
 
-            
 
-      
-          
+
+
+
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      </>
+    </>
 
 
   )
--- src/components/dashboard/Updates.js
+++ src/components/dashboard/Updates.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UpdatesData } from '../data/data';
 import './Updates.css'
 import axios from 'axios';
@@ -6,11 +6,17 @@ import { useCookies } from 'react-cookie';
 import { useEffect } from 'react';
 const Updates = () => {
   const [cookie, setCookie] = useCookies("email");
+  const [data, setData] = useState([])
+
+
   useEffect(async () => {
     const getUpdate = async() => {
       try{
         const res = await axios.get(`/tx`, {headers:{"Authorization": cookie.token}, params:{"email": cookie.email, "type":"Expense"}});
         console.log(res.data);
+        res.data.map((tx)=>{
+          setData(data.push(tx))
+        })
       }catch(err){
         console.log(err);
       }
@@ -19,17 +25,21 @@ const Updates = () => {
     await getUpdate();
   }, [])
 
+
+  
+
+
   return (
     <div className="Updates">
-      {UpdatesData.map((update) => {
+      {data.map((tx) => {
         return (
-          <div className="update">
+          <div className="Updates">
             <div className="noti">
               <div  style={{marginBottom: '0.5rem'}}>
-                <span>{update.name}</span>
-                <span> {update.noti}</span>
+                <span>{tx.category}</span>
+                <span> {tx.details}</span>
               </div>
-                <span>{update.time}</span>
+                <span>{tx.timestamps}</span>
             </div>
           </div>
         );
